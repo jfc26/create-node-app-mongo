5093be3ebec0edaa0ab70dc6cf1d892948bf1905
tuannguyen <tuannguyen7326.dev@gmail.com>
2023-06-12 01:33:45 +0700

    REMOVE - SampleController

--- src/controllers/admin-routers/index.ts
+++ src/controllers/admin-routers/index.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import loadSampleRouter from './SampleController';
 import { verifyAccessTokenMiddleware } from '../../middleware/http/verify-token-middleware';
 
 export default async function loadAdminRouter(parentRouter: Router) {
     const router = Router();
 
-    await loadSampleRouter(router);
+    // TODO -
 
     // prettier-ignore
     parentRouter.use(
